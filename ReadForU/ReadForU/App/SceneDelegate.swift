8a29f1b250d47cb0a25d63806a73f2b43787b3fc
Serena <sha0720@naver.com>
2023-10-09 22:33:54 +0900

    feat: 프로젝트파일 생성

---
+++ ReadForU/ReadForU/App/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  ReadForU
+//
+//  Created by Serena on 2023/10/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ ReadForU/ReadForU/Controller/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  ReadForU
+//
+//  Created by Serena on 2023/10/09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
