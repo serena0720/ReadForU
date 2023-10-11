e3aa16b7344f082468bc1a6311d7f17b46cb4cf8
Serena <sha0720@naver.com>
2023-10-11 10:55:31 +0900

    feat: UINavigationBar Color 지정

--- ReadForU/ReadForU/App/SceneDelegate.swift
+++ ReadForU/ReadForU/App/SceneDelegate.swift
@@ -9,15 +9,26 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let loadingViewController = LoadingViewController()
         let navigationController = UINavigationController(rootViewController: loadingViewController)
         
+        setNavigationAppearance()
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+    }
+}
+
+extension SceneDelegate {
+    private func setNavigationAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(red: 255/255, green: 90/255, blue: 93/255, alpha: 90/100)
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
