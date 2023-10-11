e21620f6747178870147984c100ef1bb26328f98
Serena <sha0720@naver.com>
2023-10-11 10:57:52 +0900

    feat: MainVC Backbutton 삭제

--- ReadForU/ReadForU/Controller/LoadingViewController.swift
+++ ReadForU/ReadForU/Controller/LoadingViewController.swift
@@ -14,7 +14,18 @@ final class LoadingViewController: UIViewController {
         view = loadingView
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
     }
 }
