107299788502bf205a38839049d9bf038fa7d8aa
Serena <sha0720@naver.com>
2023-10-13 15:19:11 +0900

    feat: Scanner Run/Stop 토글 생성

--- ReadForU/ReadForU/Controller/RealTimeTranslateViewController.swift
+++ ReadForU/ReadForU/Controller/RealTimeTranslateViewController.swift
@@ -34,6 +34,8 @@ final class RealTimeTranslateViewController: UIViewController {
         super.viewDidLoad()
         
         addChildViewController()
+        assignRealTimeTranslateViewDelegate()
+        assignDataScannerDelegate()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -52,5 +54,29 @@ final class RealTimeTranslateViewController: UIViewController {
     private func startDataScanner() {
         scannerVC.view.frame = realTimeView.scannerView.bounds
         try? scannerVC.startScanning()
+    }
+}
+
+// MARK: - Delegate
+extension RealTimeTranslateViewController: RealTimeTranslateViewDelegate {
+    private func assignRealTimeTranslateViewDelegate() {
+        realTimeView.delegate = self
+    }
+    
+    func toggleButton() {
+        if isRunning {
+            isRunning = false
+            realTimeView.pauseAndRunView.image = .init(named: "run")
+            scannerVC.stopScanning()
+        } else {
+            isRunning = true
+            realTimeView.pauseAndRunView.image = .init(named: "pause")
+            try? scannerVC.startScanning()
+        }
+    }
+}
+extension RealTimeTranslateViewController: DataScannerViewControllerDelegate {
+    private func assignDataScannerDelegate() {
+        scannerVC.delegate = self
     }
 }
