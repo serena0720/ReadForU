5e9b550715cf7b4647bcc013b6efb0b1cc30e6ec
Serena <sha0720@naver.com>
2023-10-14 01:44:53 +0900

    feat: Language Code 생성

--- ReadForU/ReadForU/Model/LanguageCode.swift
+++ ReadForU/ReadForU/Model/LanguageCode.swift
@@ -5,6 +5,10 @@
 //  Created by Serena on 2023/10/14.
 //
 
-enum LanguageCode {
-    
+enum LanguageCode: String {
+    case korean = "ko"
+    case english = "en"
+    case japanese = "ja"
+    case chinese = "zh-CN"
+    case traditionalChineseCharacters = "zh-TW"
 }
