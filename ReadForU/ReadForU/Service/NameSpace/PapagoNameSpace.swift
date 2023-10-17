9a3ee732ae9db1ef8028588e6b95d2905846e352
Serena <sha0720@naver.com>
2023-10-17 20:26:03 +0900

    feat: PapagoNameSpace 생성

---
+++ ReadForU/ReadForU/Service/NameSpace/PapagoNameSpace.swift
@@ -0,0 +1,13 @@
+//
+//  PapagoNameSpace.swift
+//  ReadForU
+//
+//  Created by Hyun A Song on 2023/10/17.
+//
+
+enum PapagoNameSpace {
+    static let contentTypeValue = "application/x-www-form-urlencoded; charset=UTF-8"
+    static let contentType = "Content-Type"
+    static let clientId = "X-Naver-Client-Id"
+    static let clientSecret = "X-Naver-Client-Secret"
+}
