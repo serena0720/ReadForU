5c18f086f1463509c20651e21d7bb9ae2ad7cbeb
Serena <sha0720@naver.com>
2023-10-18 12:11:36 +0900

    feat: LoadingView 생성

---
+++ ReadForU/ReadForU/View/LoadingView.swift
@@ -0,0 +1,60 @@
+//
+//  LoadingView.swift
+//  ReadForU
+//
+//  Created by Serena on 2023/10/10.
+//
+
+import UIKit
+import Gifu
+
+final class LoadingView: UIView {
+    private let gifImageView: GIFImageView = {
+        let image = GIFImageView()
+        image.contentMode = .scaleAspectFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpBackgroundColor()
+        configureUI()
+        setUpGifImage()
+        animateGifImage()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpBackgroundColor() {
+        backgroundColor = .systemBackground
+    }
+    
+    private func configureUI() {
+        addSubview(gifImageView)
+    }
+    
+    // MARK: - Constraints
+    private func setUpGifImage() {
+        NSLayoutConstraint.activate([
+            gifImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            gifImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            gifImageView.topAnchor.constraint(equalTo: topAnchor),
+            gifImageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    // MARK: - Private
+    private func animateGifImage() {
+        gifImageView.animate(withGIFNamed: "ReadForU",
+                             loopCount: 1,
+                             loopBlock: {
+            NotificationCenter.default.post(name: NSNotification.Name("isGifDone"),
+                                            object: nil)
+        })
+    }
+}
